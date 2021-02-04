c5fb5774871c265de1adff43d5226658c9b4cca0
Joe Malone <joe.d.malone@gmail.com>
2021-02-04 07:19:08 -0600

    added jquery library/scripting src

--- Develop/js.js
+++ Develop/js.js
@@ -1,12 +1,48 @@
 // var url = ;
 var appid = "34f9c8c9a6f0d9f8a591a875acd250b6";
 var stateCodes = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
-
 var searchCityButton = document.querySelector('.city-search-button');
 
+initializeRecentSearches();
+
+
+function initializeRecentSearches () {
+    let recentSearches = JSON.parse(localStorage.getItem('recentSearches'));        
+    if (!recentSearches) {
+        let recentSearches = {'recentSearches':
+            {
+            'recent-search-1':{'city':'','state': ''},
+            'recent-search-2':{'city':'','state': ''},
+            'recent-search-3':{'city':'','state': ''},
+            'recent-search-4':{'city':'','state': ''},
+            'recent-search-5':{'city':'','state': ''},
+            }
+        }
+        localStorage.setItem('recentSearches', JSON.stringify(recentSearches));
+        console.log(recentSearches['recentSearches'])
+    } 
+    
+}
+document.querySelector('.recent-city').addEventListener('mouseover', function(event) {
+    console.log('hello)');
+})
+
+document.getElementById('recent-city-ul').addEventListener("click", function(e) {
+    let searchCity = (e.target.innerHTML)
+    let childAttribute = (e.target.getAttribute('id'))
+    let cityStateArray = strictFormat([searchCity]);
+    let city = cityStateArray[0];
+    let state = cityStateArray[1];
+    weatherFetch(city,state,appid);
+    let recentSearches = JSON.parse(localStorage.getItem('recentSearches')
+    );
+    let updatedSearches = recentSearches['recentSearches'];
+    let newItem = updatedSearches.childAttribute = {'recentSearches':{'city':city, 'state':state}};
+    localStorage.setItem('recentSearches', JSON.stringify(newItem));
+});
 
 searchCityButton.addEventListener('click', function() {
-    var searchCity = document.querySelector('#searchBar').value;
+    let searchCity = document.querySelector('#searchBar').value;
     let cityStateArray = strictFormat([searchCity]);
     let city = cityStateArray[0];
     let state = cityStateArray[1];
@@ -14,9 +50,6 @@ searchCityButton.addEventListener('click', function() {
 })
 
 function weatherFetch(city, state,appid) {
-    // console.log(city, state)
-    // console.log(`api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${appid}`)
-    //Testing link intentionally mispelled
     console.log(String(`api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${appid}`));
     // fetch()
     //     .then(response => response.json())
@@ -29,11 +62,12 @@ function weatherFetch(city, state,appid) {
 
 
 
-//need to create an independent function that operates with a passed city name param
+//need to create an independent function that operates with passed city/state/appid params
 function strictFormat(arr) {
     // The user entry needs to be separated and formatted for fetch needs
    var newArr =arr[0].split(/(\s+)/);
    // remove any extra spaces that may be surrounding text
+        // need to resolve city names with spaces between them
    newArr = newArr.filter(function(entry) { return entry.trim() != ''; });
    // remove any commas that user may have inadvertently typed
    var finalArr = [];
