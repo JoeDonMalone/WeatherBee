8ebb1d18481cdbefc6ee99f9042d4782c9c035b7
Joe Malone <joe.d.malone@gmail.com>
2021-02-20 18:57:42 -0600

    Index function added to js

--- Develop/js.js
+++ Develop/js.js
@@ -1,5 +1,7 @@
 // var url = ;
-var appid = "34f9c8c9a6f0d9f8a591a875acd250b6";
+var currentId = "34f9c8c9a6f0d9f8a591a875acd250b6";
+var forecastId = "3b717d16ab57a50678ca7fae8c164d47";
+var uvId = "11f57e252bb7a083db68891139310d99";
 var stateCodes = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
 var searchCityButton = document.querySelector('.city-search-button');
 var todayDate = moment().format('M/D/YYYY');
@@ -58,8 +60,8 @@ searchCityButton.addEventListener('click', function() {
     let city = cityStateArray[0];
     let state = cityStateArray[1];
     let country = 'USA';
-    currentWeatherFetch(city,state,country,appid);
-    forecastWeatherFetch(city,state,country,appid);
+    currentWeatherFetch(city,state,country);
+    forecastWeatherFetch(city,state,country);
     // retrieve the storedRecentSearches for pushing
     let storedSearches = JSON.parse(localStorage.getItem('Recent Searches'));
     storedSearches.push({'search': {"city":city , "state":state }})
@@ -67,32 +69,35 @@ searchCityButton.addEventListener('click', function() {
 })
 
 
-function currentWeatherFetch(city, state, country,appid) {
+function currentWeatherFetch(city, state, country) {
     // url = `api.openweathermap.org/data/2.5/weather?q=,,&appid=`;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&units=imperial&appid=${appid}`
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&units=imperial&appid=${currentId}`
     fetch(url)
         .then(response => response.json())
         // .then(data => localStorage.setItem('apiResponseCurrentObject', JSON.stringify(data)))
         .then(data => assignCurrentWeatherData(data));// => localStorage.setItem('apiResponseObject', JSON.stringify(data)))
 }
 
-function forecastWeatherFetch(city, state, country,appid) {
+function forecastWeatherFetch(city, state, country) {
     // url = `api.openweathermap.org/data/2.5/weather?q=,,&appid=`;
-    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city},${state},${country}&cnt=48&units=imperial&appid=${appid}`
+    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city},${state},${country}&cnt=48&units=imperial&appid=${forecastId}`
     fetch(url)
         .then(response => response.json())
         // .then(data => localStorage.setItem('apiResponseFutureObject', JSON.stringify(data)))
         .then(data => assignFutureWeatherData(data));// => localStorage.setItem('apiResponseObject', JSON.stringify(data)))
 }
 
+function UVFetch(lat,lon) {
+    // url = `api.openweathermap.org/data/2.5/weather?q=,,&appid=`;
+    let url = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${uvId}`
+    fetch(url)
+        .then(response => response.json())
+        // .then(data => localStorage.setItem('UVIndexObject', JSON.stringify(data)));
+        .then(data => assignUVData(data));// => localStorage.setItem('apiResponseObject', JSON.stringify(data)))
+}
+
 
 // Decoded Items from API Response
-
-// function convertToF(kelvin) {
-//     let celsius = kelvin-273.15;
-//     // let response = JSON.parse(localStorage.getItem('apiResponseObject'));
-//     return(parseInt(celsius) * 9/5 + 32)
-// }
 
 // function determinIcon (id) {
 //     switch (id) {
@@ -103,59 +108,33 @@ function forecastWeatherFetch(city, state, country,appid) {
 //         case "03d":
 //           break;
 //         case "04d":
-//           break;
-//         case "09d":
-//           break;
-//         case "10d":
-//           break;
-//         case "11d":
-//           break;
-//         case '13d': 
-//           break;
-//         case "50d":
-//           break;
-//         case "01n":
-//           break;
-//         case "02n":
-//           break;
-//         case "03n":
-//           break;
-//         case "04n":
-//           break;
-//         case "09n":
-//           break;
-//         case '10n':
-//           break;
-//         case "11n":
-//           break;
-//         case "13n":
-//           break;
-//         case "50n":
-//           break;
-//         [default:
-//           //Statements executed when none of
-//           //the values match the value of the expression
-//           break;]
 //       }
 // }
 
 // CURRENT Weather Function//
 function assignCurrentWeatherData(response) {
     let cityName = response.name;
-    // let iconimg = repsonse.weather[0].icon;
     let humidity = response.main.humidity;
     let windSpeed = response.wind.speed;
     let temperature = response.main.temp;
     let imgIcon = response.weather[0].icon;
-    // let holderImgSRC = `http://openweathermap.org/img/wn/${imgIcon}@2x.png`;
-    console.log(temperature);
-    // let uvIndex= ;
+    let lat = [response.coord.lat];
+    let lon = [response.coord.lon];
+    UVFetch(lat,lon);
+
     $('.city-name').text(`${cityName} (${todayDate})`);
-    $('.city-name').append(`<img src='http://openweathermap.org/img/wn/${imgIcon}@2x.png'>`);
+    $('.city-name').append(`<img src='http://openweathermap.org/img/wn/${imgIcon}@2x.png' class = 'current-img'>`);//
     $('.temperature').text(`Temperature: ${temperature}${String.fromCharCode(176)}F`);
     $('.humidity').text(`Humidity: ${humidity}%`);
     $('.wind-speed').text(`Wind Speed: ${windSpeed} MPH`);
-    // $('.uv-index').text(`UV Index: ${cityName}`);
+    // 
+}
+
+// UV Data Function 
+
+function assignUVData(response) {
+    let uvIndex = response.value;
+    $('.uv-index').text(`UV Index: ${uvIndex}`);
 }
 
 // FUTURE WEATHER FORECAST //
@@ -164,42 +143,19 @@ function assignFutureWeatherData(response) {
     $('.card ').each( function (i) {
         if(i==0) {
             dateItemIndex = 5;
-        } else{
-            dateItemIndex+=8
+        } else{ dateItemIndex+=8
         }
-        console.log('Loop Index: ', i, 'Card Index: ', dateItemIndex);
+
         let date = moment().add(i+1, 'days').format('M/D/YYYY');
         let temperature = response.list[dateItemIndex].main.temp;
         let humidity = response.list[i].main.humidity;
         let imgIcon = response.list[dateItemIndex].weather[0].icon;
 
-
         $(this).find('.future-date').text(date);
-        $(this).find('.future-outlook-image').append(`<img src='http://openweathermap.org/img/wn/${imgIcon}@2x.png'>`)
-        // $(this).find().text(response.list[5].weather[0].icon); //Future forecast Image... sunny/cloudy/rainy
+        $(this).find('img').attr('src', `http://openweathermap.org/img/wn/${imgIcon}@2x.png`).css("visibility", "visible")
         $(this).find('.future-temperature').text(`Temperature: ${temperature}${String.fromCharCode(176)}F`); 
         $(this).find('.future-humidity').text(`Humidity: ${humidity}%`); 
-        
-
-        // $(this).find('.card-text').text(todayDate);
     })
-
-
-    // for(let i = 5;i<=45; i+8){
-    //     
-    //     
-    //     // let secondsInDay = 86400;
-    //     let dayOne = response.list[5];
-    //     // let dayTwo = dayOne - (86400); 
-    //     // let dayThree = dayOne - (86400 * 2);
-    //     // let dayFour = dayOne - (86400 * 3);
-    //     // let dayFive = dayOne - (86400 * 4);
-    //     $('.city-name').text(`${cityName} (${todayDate})`);
-    //     $('.temperature').text(`Temperature: ${temperature}${String.fromCharCode(176)}F`);
-    //     $('.humidity').text(`Humidity: ${humidity}%`);
-    //     $('.wind-speed').text(`Wind Speed: ${windSpeed} MPH`);
-    //     // $('.uv-index').text(`UV Index: ${cityName}`);
-    // }
 }
 
 //need to create an independent function that operates with passed city/state/appid params
